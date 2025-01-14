af73b4f00624c843a02c9b50ba5837ed7a0bfaed
Mārtiņš Kristaps Mickus <64271878+MartinsKMickus@users.noreply.github.com>
2025-01-14 23:53:18 +0200

    Possible network header structure

--- network.cuh
+++ network.cuh
@@ -6,10 +6,26 @@ struct Neuron
     float v; // Membrane potential
     float u; // Recovery variable
     float a, b, c, d; // Parameters
-    // Excitatory and inhibitory connections?
+    // I will be calculated later
 };
 
 struct Neuron *neurons; // Neurons represented as an array
+int neurons_count;
 
 // Connections represented as a matrix
 int **connections;
+
+void add_neuron(int index, float v, float a, float b, float c, float d);
+void add_connection(int from, int to, int latency, float weight);
+// TODO: Defife input and output neurons?
+
+__device__ void update_neuron(Neuron *neurons);
+
+float get_step_performance();
+
+void simulate_step();
+void start_simulation();
+void stop_simulation();
+
+
+void free_network();
